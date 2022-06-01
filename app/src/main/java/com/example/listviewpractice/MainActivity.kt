99e093d278e8f55019ed06f35e907507104abf6b
seulali <seulali79@gmail.com>
2022-06-01 17:49:05 +0900

    출생년도로 한국식 나이(2022년 기준)으로 보여주기

--- app/src/main/java/com/example/listviewpractice/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice/MainActivity.kt
@@ -2,23 +2,32 @@ package com.example.listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
+import com.example.listviewpractice.adapters.StudentAdapter
 import com.example.listviewpractice.datas.Student
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     val mStudentList = ArrayList<Student>()
+    lateinit var mAdapter: StudentAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mStudentList.add.(Student("임슬아",1993))
-        mStudentList.add.(Student("김시은",1993))
-        mStudentList.add.(Student("서주영",1993))
-        mStudentList.add.(Student("정유석",1993))
-        mStudentList.add.(Student("박진",1993))
-        mStudentList.add.(Student("이주연",1993))
-        mStudentList.add.(Student("강선아",1993))
+        mStudentList.add( Student("임슬아",1993))
+        mStudentList.add( Student("김시은",1993))
+        mStudentList.add( Student("서주영",1991))
+        mStudentList.add( Student("정유석",1990))
+        mStudentList.add( Student("박진",1992))
+        mStudentList.add( Student("이주연",1993))
+        mStudentList.add( Student("강선아",1993))
+
+        mAdapter = StudentAdapter(this,R.layout.student_list_item,mStudentList)
+
+        studentListView.adapter = mAdapter
+
 
 
     }
